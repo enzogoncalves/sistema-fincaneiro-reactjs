a6953ec4aca934344be8a9a754051a92d0205b2b
enzogoncalves <enzomateusgonc@gmail.com>
2022-12-15 12:56:42 -0300

    ...

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import {App} from './App'
+import { inject } from '@vercel/analytics';
+
+inject();
 
 const root = createRoot(document.querySelector("#root"))
 
